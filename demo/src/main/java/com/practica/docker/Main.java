25830a8904fff67af4bb21cd7e32cdc6d28dd470
Diego <diego.romero.vera.11@gmail.com>
2025-09-28 12:11:31 -0500

    Primera consulta realizada

--- demo/src/main/java/com/practica/docker/Main.java
+++
@@ -1,7 +0,0 @@
-package com.practica.docker;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ demo/src/main/java/es/tierno/parcticajavabbd/Main.java
@@ -0,0 +1,22 @@
+package es.tierno.parcticajavabbd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/midatabase?useSSL=false&serverTimezone=UTC","root","rootpass");
+            java.sql.Statement stmt=con.createStatement();
+
+            ResultSet rs=((java.sql.Statement) stmt).executeQuery("select * from productos");
+            while(rs.next())
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+            con.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}

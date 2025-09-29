8ee841abf21dba84a403ea2dc2eff110be965f3a
Diego <diego.romero.vera.11@gmail.com>
2025-09-29 12:18:29 -0500

    segunda version

--- demo/src/main/java/es/tierno/parcticajavabbd/Main.java
+++ demo/src/main/java/es/tierno/parcticajavabbd/Main.java
@@ -5,20 +5,21 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        try{
+    public static void main(String[] args) throws Exception {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/midatabase?allowPublicKeyRetrieval=true&useSSL=false";
             Connection con = DriverManager.getConnection(url, "root", "rootpass");
 
-            // onnection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/midatabase?useSSL=false&serverTimezone=UTC","root","rootpass");
-            java.sql.Statement stmt=con.createStatement();
+            // Connection
+            // con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/midatabase?useSSL=false&serverTimezone=UTC","root","rootpass");
+            java.sql.Statement stmt = con.createStatement();
 
-            ResultSet rs=((java.sql.Statement) stmt).executeQuery("select * from productos");
-            while(rs.next())
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+            ResultSet rs = ((java.sql.Statement) stmt).executeQuery("select * from productos");
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
             con.close();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }

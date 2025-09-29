89d0f9fa6ff81b323aff241f2af86f03f65554cd
Diego <diego.romero.vera.11@gmail.com>
2025-09-29 12:16:30 -0500

    carpeta del proyecto bien estructurada

--- demo/src/main/java/es/tierno/parcticajavabbd/Main.java
+++ demo/src/main/java/es/tierno/parcticajavabbd/Main.java
@@ -8,7 +8,10 @@ public class Main {
     public static void main(String[] args) throws Exception{
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/midatabase?useSSL=false&serverTimezone=UTC","root","rootpass");
+            String url = "jdbc:mysql://localhost:3306/midatabase?allowPublicKeyRetrieval=true&useSSL=false";
+            Connection con = DriverManager.getConnection(url, "root", "rootpass");
+
+            // onnection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/midatabase?useSSL=false&serverTimezone=UTC","root","rootpass");
             java.sql.Statement stmt=con.createStatement();
 
             ResultSet rs=((java.sql.Statement) stmt).executeQuery("select * from productos");
